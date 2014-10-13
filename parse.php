1eccd734863c659868879b9636027e3814659f48
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-10-14 01:25:12 +0900

    Security For Other Objects

--- parse.php
+++ parse.php
@@ -5,11 +5,15 @@ require 'vendor/autoload.php';
 use Parse\ParseClient;
 use Parse\ParseUser;
 use Parse\ParseException;
+use Parse\ParseObject;
+use Parse\ParseACL;
  
 ParseClient::initialize('LicvGZYQ3x9rDtfiDnaNy42GmIJdP0TuoVBJBFZi', 'QmaCbGyfU0chwYJ4n77cM1lv3pZeeVxNfa0FGrLE', 'pwLv0m1SioJBnuqlI3mvZ0Cv6jDRoC0BIRImMgGO');
 
 
 // == 
+
+// login
 try {
     $user = ParseUser::logIn("my name", "my pass");
     // Do stuff after successful login.
@@ -17,10 +21,7 @@ try {
     // The login failed. Check error to see why.
 }
 
-$currentUser = ParseUser::getCurrentUser();
-if ($currentUser) {
-    // do stuff with the user
-    print_r($currentUser);
-} else {
-    // show the signup or login page
-}
+$privateNote = new ParseObject("Note");
+$privateNote->set("content", "This note is private!");
+$privateNote->setACL(ParseACL::createACLWithUser(ParseUser::getCurrentUser()));
+$privateNote->save();
