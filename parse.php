a27923386c142ce84b698c7c37a842964381e42b
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-10-14 01:12:58 +0900

    user sign up

--- parse.php
+++ parse.php
@@ -3,20 +3,29 @@
 require 'vendor/autoload.php';
  
 use Parse\ParseClient;
+use Parse\ParseUser;
+use Parse\ParseException;
  
 ParseClient::initialize('LicvGZYQ3x9rDtfiDnaNy42GmIJdP0TuoVBJBFZi', 'QmaCbGyfU0chwYJ4n77cM1lv3pZeeVxNfa0FGrLE', 'pwLv0m1SioJBnuqlI3mvZ0Cv6jDRoC0BIRImMgGO');
 
 
 // == 
 
-$query = new Parse\ParseQuery("GameScore");
 
+$user = new ParseUser();
+$user->set(",sername", "my name");
+$user->set("password", "my pass");
+$user->set("email", "email@example.com");
+ 
+// other fields can be set just like with ParseObject
+$user->set("phone", "415-392-0202");
+ 
 try {
-      $gameScore = $query->get("u2uyQRLD7d");
-      $gameScore->delete("playerName");
-      $gameScore->save();
-      // The object was retrieved successfully.
+  $user->signUp();
+  // Hooray! Let them use the app now.
 } catch (ParseException $ex) {
-      // The object was not retrieved successfully.
-      // error is a ParseException with an error code and message.
+  // Show the error message somewhere and let the user try again.
+  echo "Error: " . $ex->getCode() . " " . $ex->getMessage();
 }
+
+
